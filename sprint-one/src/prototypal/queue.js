85040b250598d48cf1d171eaa665946ff5e5d8b2
Han Kim <hankyulkim93@gmail.com>
2019-07-18 10:59:53 -0700

    passed tests for prototypal

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var sameInstance = Object.create(queueMethods);
+  sameInstance.storage = {};
+  sameInstance.lastCount = 0;
+  sameInstance.startCount = 0;
+
+  return sameInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.lastCount] = value;
+    this.lastCount++;
+  },
+
+  dequeue: function() {
+    var value = this.storage[this.startCount];
+    delete this.storage[this.startCount];
+    this.startCount++;
+    return value;
+  },
+
+  size: function() {
+    if (this.lastCount - this.startCount < 0) {
+      return 0;
+    } else {
+      return (this.lastCount - this.startCount);
+    }
+  }
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,51 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var sameInstance = Object.create(stackMethods);
+  sameInstance.storage = {};
+  sameInstance.count = 0;
+
+  return sameInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  },
 
+  pop: function() {
+    if (this.count > 0) {
+      this.count--;
+      var value = this.storage[this.count];
+      delete this.storage[this.count];
+      return value;
+    }
+  },
+
+  size: function() {
+    return this.count;
+  }
+};
+
+
+// Stack.stackMethods = {};
+
+// Stack.stackMethods.push = function(value) {
+//   this.storage[this.count] = value;
+//   this.count++;
+// };
+
+// Stack.stackMethods.pop = function() {
+//   if (this.count > 0) {
+//     this.count--;
+//     var value = this.storage[this.count];
+//     delete this.storage[this.count];
+//     return value;
+//   }
+// };
+
+// Stack.stackMethods.size = function() {
+//   return this.count;
+// };
 

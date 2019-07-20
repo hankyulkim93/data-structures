31855f8c9e563153e83b2aa5feef184a94a376ef
hankyulkim93 <hankyulkim93@gmail.com>
2019-07-19 21:52:39 -0700

    passed tests for linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,46 +2,42 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
-  list.length = 0;
   
   list.addToTail = function(value) { // add node & set it to tail.
   //LinkedList tail should equal to whatever node(w/ value) is being added.
-    // if (list.head = null) { // if very first node added, then give value to tail and equal it to head
-
-    // var oldTail = list.tail;
-    // list.tail = Node(value);
-    // if (list.head = null) {
-    //   list.head = list.tail;
-    //   list.head.next = oldTail;
-    // } else if (list.head != null) {
-    //   oldTail.next = list.tail;
-    // }
+  
+    var newNode = Node(value);
     
-    //   var newHead = list.tail;
-    //   var oldTail = list.tail;
-    // } else { // if NOT very first node added, then need to give value to tail node but not overwrite previous tail PLUS give head non-null value.
-    //   list.head = newHead;
-    //     if (list.length === 1) {
-    //       list.tail = Node(value);
-    //       list.head.next = list.tail;
-    //       // var bleh = list.tail.next;
-    //     } else if (list.length > 1) {
-    //       list.tail.next = Node(value);
-    //       list.tail = list.tail.next;
-    //     }
-    // }
-    list.length ++; // add count to list (UNNECESSARY?)
+    if (list.head === null) {
+      list.head = newNode;
+    } else {
+      list.tail.next = newNode;
+    }
+    list.tail = newNode;
+
   };
 
   list.removeHead = function() { //remove node that head is pointing to and redirect head to next node in sequence (if applicable).
       // var tempVar = list.head.value; //need figure out how to return just the value of list.head
-      // delete list.head;
-    var tempVar = list.head.value;
-    return tempVar;
-    list.length --;
+    var oldHead = list.head.value;
+    list.head = list.head.next;
+    
+    return oldHead;
+
   };
 
   list.contains = function(target) {
+  
+    var currNode = list.head;
+    
+    while (currNode) {
+      if (target === currNode.value) {
+        return true;
+      } else {
+        currNode = currNode.next;
+      }
+    }
+    return false;
   };
 
   return list;
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,21 +3,54 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+  //newTree.children is an array.
+
+  extend(newTree, treeMethods);
 
   return newTree;
+};
+
+var extend = function(to, from ) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  //get Tree(value)
+  //push Tree(value) to children array
+  var newNode = Tree(value);
+  this.children.push(newNode);
 };
 
 treeMethods.contains = function(target) {
+
+  if (target === this.value) {
+    return true;
+  } else {
+    for (var i = 0; i < this.children.length; i++) { 
+      if (target === this.children[i].value || this.children[i].contains(target) === true) {
+        return true;
+      } else {
+        this.children[i].contains(target);
+      }
+    }
+  }
+  return false;
 };
 
+var tree = Tree();
 
-
+tree.addChild(5); ``
+tree.addChild(6);
+tree.children[0].addChild(7);
+tree.children[1].addChild(8);
+tree.contains(7);
+tree.contains(8);
+// we could use recursion to improve this
 /*
  * Complexity: What is the time complexity of the above functions?
  */
